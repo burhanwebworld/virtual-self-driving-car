0a871d80b6987752e6655b69c4045dd5de776499
burhanwebworld <burhan102957@gmail.com>
2021-06-17 22:26:53 +0600

    first update

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
-num1=int(input("Enter the first number: "))
-#input value for variable num1.
-num2=int(input("Enter the second number: "))
-#input value for variable num2.
-mul=num1*num2;
-#perform multiplication operation.
-print("the product of given numbers is: ",mul)
-#display the product.
+import pygame
+pygame.init()
+window = pygame.display.set_mode((1200, 400))
+track = pygame.image.load('track2.png')
+car = pygame.image.load('tesla.png')
+car = pygame.transform.scale(car, (30, 60))
+car_x = 150
+car_y = 300
+focal_dis = 25
+drive = True
+clock = pygame.time.Clock()
+while drive:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            drive = False
+    clock.tick(60)
+    cam_x = car_x + 15
+    cam_y = car_y + 15
+    up_px = window.get_at((cam_x, cam_y - focal_dis))[0]
+    print(up_px)
+    if up_px == 255:
+        car_y = car_y - 2
+    window.blit(track, (0, 0))
+    window.blit(car, (car_x, car_y))
+    pygame.draw.circle(window, (0, 255, 0), (cam_x, cam_y), 5, 5)
+    pygame.display.update()
